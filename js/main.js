75067cd5ca279de943b7f319d61188b66f94bff0
kucheriavii <kucheriavii@gmail.com>
2017-03-03 02:10:03 +0200

    brutal work doned|fixes

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 var menuSlider = document.getElementById('menu-scroller');
 
 noUiSlider.create(menuSlider, {
-	start: 10,
+	start: 1,
 	behaviour: 'tap',
 	connect: [true, false],
 	range: {
@@ -18,7 +18,10 @@ menuSlider.noUiSlider.on('update', function( values, handle ){
 	menuField.innerHTML = Number(values[handle]);
 	$ulPosition = Number($('#menu-scroller-value').value);
 	menuBlockTop = Number(values[handle])*-1+"%";
-	$(".menu-block").css('top', menuBlockTop)
+	$('.menu-block').css('top', menuBlockTop)
 
 });
 /***************************************************************************/
+$('.menu button').click(function(){
+	$('.fixes').toggleClass('disabled')
+})
